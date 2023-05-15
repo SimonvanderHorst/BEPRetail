fa22a8a5fa6a82a60a952c2bf953fe395092d6d5
SimonvanderHorst <s.h.vanderhorst@student.tudelft.nl>
2023-05-15 11:07:59 +0200

    hi

--- Agent.py
+++ Agent.py
@@ -1,59 +1,92 @@
 import numpy as np
 import random
 from mesa import Agent
+from mesa import Model
+from mesa.space import SingleGrid
 
 
 class Food(Agent):
-    def __init__(self, pos, model, probability_range):
+    def __init__(self, pos, model, probability_range, steps_until_expiration, food_price):
         super().__init__(pos, model)
+        self.breed = "Food"
         self.pos = pos
         self.model = model
         self.probability_range = probability_range
-        self.days_until_expiration = 10
+        self.steps_until_expiration = steps_until_expiration
         self.food_type = 'meat'
-        self.price = 1
+        self.food_price = food_price
         self.purchased = 0
+        self.minimum_day_until_expiry = 5  # check what this does again
+        self.expired = 0
 
     def step(self):
-        self.days_until_expiration -= 1
+        self.steps_until_expiration -= 1
+        if self.steps_until_expiration == -1:
+            self.expired = 1
 
 
 class Consumer(Agent):
     def __init__(self, pos, model, family, wealth):
         super().__init__(pos, model)
+        self.breed = "Consumer"
         self.pos = pos
         self.model = model
         self.family_size = family
         self.wealth = wealth
-        self.minimum_days_until_expiry = 10/self.family_size #lineair?
+        self.minimum_days_until_expiry = 10 / self.family_size  # lineair?
 
     def step(self):
-        neighbors = self.model.grid.get_neighbors(self.pos, moore=False)
-        for neighbor in neighbors:
-            if neighbor.food_type == 'meat':
-                neighbor.purchased = 1
+
+        self.update_neighbors()  # run update_neighbors
+
+        for neighbor in self.neighbors:
+            if neighbor.breed == "Food" and neighbor.expired == 0:
+                if neighbor.food_type == 'meat':
+                    neighbor.purchased = 1
+                elif neighbor.food_type == "vegetable":
+                    # if a random integer between 0 and the products base expiry date is lower than its current
+                    # expiry date the consumer will purchase the item
+                    if self.random.randint(0, neighbor.steps_until_expiration) <= neighbor.minimum_day_until_expiry:
+                        neighbor.purchased = 1
+
+        # consumer agent movement todo: fix swap_pos attribute
+
+        for neighbor in self.neighbors:
+            if neighbor.breed == "Consumer" and random.random() < 0.3: #todo hardcoded swap number
+                self.model.grid.swap_pos(self, neighbor)
+                print("je")
+
+
+        if self.empty_neighbors:
+            new_pos = self.random.choice(self.empty_neighbors)  # find a random empty neighbor position
+            self.model.grid.move_agent(self, new_pos)  # move to that position
+
+
+    def update_neighbors(self):
+        """
+        Look around and see who my neighbors are.
+        """
+        self.neighborhood = self.model.grid.get_neighborhood(self.pos, moore=False,
+                                                             radius=1)  # returns a list of cells that are in the
+        # neighborhood of a certain point.
+        self.neighbors = self.model.grid.get_neighbors(self.pos, moore=False,
+                                                       radius=1)  # returns a list of objects that are in the
+        # neighborhood of a certain point.
+
+        self.empty_neighbors = [c for c in self.neighborhood if
+                                self.model.grid.is_cell_empty(c)]  # finds and puts the empty neighbors in a list
+        # is_cell_empty is a method from Grid in Mesa. It returns True if the cell is empty.
 
 
 class Retailer(Agent):
     def __init__(self, pos, model, waste_awareness):
         super().__init__(pos, model)
+        self.breed = "Retailer"
         self.waste_awareness = waste_awareness
+        self.advertisement = 1
 
     def step(self):
         neighbors = self.model.grid.get_neighbors(self.pos, moore=False)
-        for neighbor in neighbors:
-            neighbor.minimum_day_until_expiry +=2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for agent in neighbors:
+            if agent.breed == "Food":
+                agent.steps_until_expiration += 1
--- Model.py
+++ Model.py
@@ -11,43 +11,87 @@ from Agent import Food, Retailer, Consumer
 
 
 class RetailerWaste(Model):
-    def __init__(self, width=100, height=100, retailer_probability_range=3, ):
+    def __init__(self, width=30, height=30, food_density=0.8, steps_until_expiration=random.randint(20, 40),
+                 retailer_density=0.2, consumer_density=0.3, food_type_probability=0.5, food_price=10):
+        # adjust these variables at retailmodel level, this is the base scenario
 
-        self.width = width
-        self.height = height
+        self.width = width  # width of the model
+        self.height = height  # height of the model
+        self.food_density = food_density  # %chance of block being food
+        self.steps_until_expiration = steps_until_expiration  # model steps until food is expired
+        self.retailer_density = retailer_density  # %chance of block being a retailer agent
+        self.consumer_density = consumer_density  # %chance of a block being a consumer agent
+        self.food_type_prob = food_type_probability  # %chance of a food agent being vegetable
+        self.food_price = food_price
         self.schedule = RandomActivation(self)
-        self.grid = SingleGrid(width, height, torus=False)
+        self.grid = SingleGrid(width, height, torus=False)  # each block can only contain one agent,
+        # the edges of the model act like walls
 
-        for x in range(self.width):
-            for y in range(self.height):
-                if random.randint(0, retailer_probability_range) == 1:
-                    new_food = Food((x, y), self, retailer_probability_range)
-                    self.grid.position_agent(new_food, x, y)
-                    self.schedule.add(new_food)
+        for y in range(self.height):
+            for x in range(self.width):
+                # every block in every other row has chance(food_density) of becoming food
+                if (y % 2) == 0 and self.random.random() < self.food_density:
+                    new_food = Food((x, y), self, food_density,
+                                    steps_until_expiration=random.randint(10, steps_until_expiration),
+                                    food_price=food_price)  # TODO: define this + hardcoded number + Paul vragen?
+                    self.grid.position_agent(new_food, x, y)  # position the agent in the grid
+                    self.schedule.add(new_food)  # add agent to the scheduler
+                    if self.random.random() <= food_type_probability:  # odds of the product being either meat or vegetable
+                        new_food.food_type = "meat"
+                    else:
+                        new_food.food_type = "vegetable"
+
+
+                elif (
+                        y % 2) == 0 and self.random.random() < self.retailer_density:  # if the aisle is still empty a retail can place an advert
+                    new_retailer = Retailer((x, y), self, retailer_density)
+                    self.grid.position_agent(new_retailer, x, y)
+                    self.schedule.add(new_retailer)
+                elif self.random.random() < self.consumer_density:  # Set chance consumers are spawned on the aisle
+                    new_consumer = Consumer((x, y), self, consumer_density, wealth=20)
+                    self.grid.position_agent(new_consumer, x, y)
+                    self.schedule.add(new_consumer)
 
     def step(self):
-        '''
+        """
         Time in model moves in steps
-        '''
+        """
         self.schedule.step()
+
         # self.datacollector.collect(self)
         # print(self.grid.find_empty())
 
 
-retailmodel = RetailerWaste(width=100, height=100, retailer_probability_range=1)
+# this is where you update the model parameters
+retailmodel = RetailerWaste(width=40, height=40, food_density=0.7, steps_until_expiration=random.randint(20, 40),
+                            retailer_density=0.1, consumer_density=0.1, food_type_probability=0.5, food_price=random.randint(8,10))
 
 
 # color setup for holoviews
 
 def value(cell):
     count = 0
-    if cell is None:
-        return 50
-    if cell.food_type == 'meat':
-        return 20
+    if cell is not None:  # logic to identify agents and corresponding attributes and give them a color
+        if cell.breed == "Food":
+            if cell.purchased == 0 and cell.expired == 0:
+                if cell.food_type == "meat":
+                    return round(0.5 * cell.steps_until_expiration)  # red
+                if cell.food_type == "vegetable":
+                    return 80 - round(0.5 * cell.steps_until_expiration)  # green
+            elif cell.purchased == 1:
+                return 20  # orange
+            elif cell.expired == 1:
+                return 40  # lightorange
+
+        if cell.breed == "Retailer" and cell.advertisement == 1:
+            return 80  # blue
+        if cell.breed == "Consumer":
+            return 100  # purple
+        return
+    return 50  # yellow if none
 
 
-# used to get a subset of available colormaps for holoviews
+# used to get a subset of available colormaps for holoviews, copy pasted from a different model
 def format_list(l):
     return (' '.join(sorted([k for k in l])))
 
@@ -58,18 +102,17 @@ hv.extension('bokeh')
 hmap = hv.HoloMap()
 
 
-def run_model():
-    for i in range(50):
+def run_model():  # defining the run_model class
+    for i in range(50):  # steps the model takes
         retailmodel.step()
-        data = np.array([[value(c) for c in row] for row in retailmodel.grid.grid])
-        data = np.transpose(data)
+        data = np.array([[value(retailmodel.grid[(x, y)]) for x in range(retailmodel.grid.height)] for y in range(retailmodel.grid.height)])
         data = np.flip(data, axis=0)
-        bounds = (0, 0, 5, 5)
+        bounds = (0, 0, 2, 2)
 
         hmap[i] = hv.Image(data, vdims=[hv.Dimension('a', range=(0, 100))], bounds=bounds).relabel(
-            'Plastic in river').opts(
+            'Retailer Model').opts(
             cmap='Spectral', xticks=[0], yticks=[0])  # andere optie RdYlBu
-    hmap
+    # hmap
     # show visualiser, bugs out when it's run before the batchrunner
     pn.panel(hmap).show()
     model_data = retailmodel.datacollector.get_model_vars_dataframe()
